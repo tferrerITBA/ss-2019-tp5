1b3f480e33ceacac988bc07bbbe7897f70b459f3
tferrerITBA <tferrer@itba.edu.ar>
2019-10-08 10:30:07 -0300

    Format cell correctly in ovito

--- render.py
+++ render.py
@@ -2,9 +2,19 @@ from ovito.io import import_file
 from ovito.vis import VectorDisplay
 from ovito.modifiers import *
 
-node = import_file("C:\\Users\\Marcos\\git\\ss-2019-tp5\\ovito_output.xyz", multiple_frames = True, columns = 
-	['Particle Identifier', 'Radius', 'Position.X', 'Position.Y', 'Velocity.X', 'Velocity.Y',
-	 'Color.R', 'Color.G', 'Color.B'])
+node = import_file("ovito_output.xyz", multiple_frames = True, columns = 
+	['Particle Identifier', 'Radius', 'Position.X', 'Position.Y', 'Velocity.X', 'Velocity.Y'])
 # id:I:1:radius:R:1:pos:R:2:velo:R:2:color:R:3
 node.add_to_scene()
+
+cell = node.source.cell
+mat = cell.matrix.copy()
+# cell vectors
+mat[0][0] = 0.4 # width
+mat[1][1] = 1.5 # height
+# cell origin
+mat[0][3] = 0 # X origin
+mat[1][3] = 0 # Y origin
+
+cell.matrix = mat
 node.compute()
